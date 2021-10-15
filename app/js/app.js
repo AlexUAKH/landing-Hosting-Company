af4b8017a12013ab038787245bcde3d6603b55b4
AlexUAKH <sashaos1404@gmail.com>
2021-10-15 12:10:28 +0300

    intro section done

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,34 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.getElementById("burger");
+  const menu = document.getElementById("menu");
+  const bodyNode = document.body || document.getElementsByTagName("body")[0];
+  let documentWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
+  if (documentWidth < 960) {
+    moveNode("intro__img", "intro__body", 1);
+  }
 
-	// Custom JS
+  burger.addEventListener("click", toggleBuger);
+  window.addEventListener("resize", function () {
+    documentWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
+    console.log(documentWidth);
+    if (documentWidth < 960) {
+      moveNode("intro__img", "intro__body", 1);
+    }
+  });
 
-})
+  function toggleBuger() {
+    burger.classList.toggle("active");
+    menu.classList.toggle("active");
+    bodyNode.classList.toggle("lock");
+  }
+});
+
+function moveNode(node, targetNode, targetOrder = 0) {
+  const fragment = document.getElementsByClassName(node)[0];
+  document
+    .getElementsByClassName(targetNode)[0]
+    .children[targetOrder].before(fragment);
+}
